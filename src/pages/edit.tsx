8ea4986afb99194f89317bc7a47227a43e98533c
ot07 <ot07neoki@gmail.com>
2023-02-28 23:38:12 +0900

    switchable to edit, split, and preview mode

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -7,12 +7,73 @@ import {
 } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition, RadioGroup } from '@headlessui/react'
 import { Fragment, useCallback, useRef, useState } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
 import Split from 'react-split'
 import { MarkdownRenderer } from '~/components/Renderer'
 import { useGlobalPointerUpEvent } from '~/hooks/useGlobalPointerUpEvent/useGlobalPointerUpEvent'
+
+type ContentIconKey = 'edit' | 'split' | 'preview'
+const contentIcons: { [K in ContentIconKey]: JSX.Element } = {
+  edit: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-align-justified"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M4 6l16 0"></path>
+      <path d="M4 12l16 0"></path>
+      <path d="M4 18l12 0"></path>
+    </svg>
+  ),
+  split: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-layout-columns"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+      <path d="M12 4l0 16"></path>
+    </svg>
+  ),
+  preview: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-photo"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M15 8l.01 0"></path>
+      <path d="M4 4m0 3a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v10a3 3 0 0 1 -3 3h-10a3 3 0 0 1 -3 -3z"></path>
+      <path d="M4 15l4 -4a3 5 0 0 1 3 0l5 5"></path>
+      <path d="M14 14l1 -1a3 5 0 0 1 3 0l2 2"></path>
+    </svg>
+  ),
+}
 
 type FormValues = {
   title: string
@@ -30,6 +91,7 @@ const Edit: NextPage = () => {
   const [isDraft, setIsDraft] = useState(false)
   const isDraftRef = useRef(false)
   const [gutterIsActive, setGutterIsActive] = useState(false)
+  const [selected, setSelected] = useState<ContentIconKey>('split')
 
   const handlePointerUp = useCallback(() => {
     setGutterIsActive(false)
@@ -269,34 +331,90 @@ const Edit: NextPage = () => {
                     >
                       Content
                     </label>
-                    <Split
-                      className="relative mt-1 flex flex-1 overflow-hidden rounded-md border border-slate-300"
-                      gutterSize={1}
-                      gutter={() => {
-                        const gutter = document.createElement('div')
-                        gutter.className =
-                          'relative bg-slate-300 after:absolute after:-left-1 after:z-10 after:block after:h-full after:w-[9px] hover:cursor-col-resize hover:bg-blue-300 hover:ring-2 active:bg-blue-300 active:ring-2'
-                        gutter.addEventListener('pointerdown', () => {
-                          setGutterIsActive(true)
-                        })
-                        return gutter
-                      }}
-                    >
-                      <textarea
-                        className={`${
-                          gutterIsActive ? 'scrollbar-none' : ''
-                        } block w-full resize-none overflow-scroll rounded-l-md p-4 outline-none ring-0`}
-                        placeholder="Post content here..."
-                        {...register('content')}
-                      />
-                      <div
-                        className={`${
-                          gutterIsActive ? 'scrollbar-none' : ''
-                        } w-full overflow-scroll p-4`}
+                    <div className="mt-1 flex flex-1 flex-col">
+                      <RadioGroup
+                        value={selected}
+                        onChange={setSelected}
+                        className="flex space-x-2"
                       >
-                        <MarkdownRenderer content={content} />
-                      </div>
-                    </Split>
+                        {Object.entries(contentIcons).map(([key, icon]) => (
+                          <RadioGroup.Option
+                            key={key}
+                            value={key}
+                            className={({ active, checked }) =>
+                              `${active ? '' : ''}
+                  ${
+                    checked
+                      ? 'bg-[#0f83fd] bg-opacity-75 text-white'
+                      : 'bg-slate-100'
+                  }
+                    flex cursor-pointer items-center justify-center rounded-md border border-slate-200 p-1.5 focus:outline-none`
+                            }
+                          >
+                            {({ checked }) => (
+                              <div
+                                className={`${
+                                  checked ? 'text-white' : 'text-slate-500'
+                                } flex h-5 w-5 items-center justify-center`}
+                              >
+                                {icon}
+                              </div>
+                            )}
+                          </RadioGroup.Option>
+                        ))}
+                      </RadioGroup>
+
+                      {selected === 'edit' && (
+                        <textarea
+                          className={`${
+                            gutterIsActive ? 'scrollbar-none' : ''
+                          } mt-2 block w-full flex-1 resize-none overflow-scroll rounded-md rounded-l-md border border-slate-300 p-4 outline-none ring-0`}
+                          placeholder="Post content here..."
+                          {...register('content')}
+                        />
+                      )}
+
+                      {selected === 'split' && (
+                        <Split
+                          className="relative mt-2 flex flex-1 overflow-hidden rounded-md border border-slate-300"
+                          gutterSize={1}
+                          gutter={() => {
+                            const gutter = document.createElement('div')
+                            gutter.className =
+                              'relative bg-slate-300 after:absolute after:-left-1 after:z-10 after:block after:h-full after:w-[9px] hover:cursor-col-resize hover:bg-blue-300 hover:ring-2 active:bg-blue-300 active:ring-2'
+                            gutter.addEventListener('pointerdown', () => {
+                              setGutterIsActive(true)
+                            })
+                            return gutter
+                          }}
+                        >
+                          <textarea
+                            className={`${
+                              gutterIsActive ? 'scrollbar-none' : ''
+                            } block w-full resize-none overflow-scroll rounded-l-md p-4 outline-none ring-0`}
+                            placeholder="Post content here..."
+                            {...register('content')}
+                          />
+                          <div
+                            className={`${
+                              gutterIsActive ? 'scrollbar-none' : ''
+                            } w-full overflow-scroll p-4`}
+                          >
+                            <MarkdownRenderer content={content} />
+                          </div>
+                        </Split>
+                      )}
+
+                      {selected === 'preview' && (
+                        <div
+                          className={`${
+                            gutterIsActive ? 'scrollbar-none' : ''
+                          } mt-2 w-full flex-1 overflow-scroll rounded-md border border-slate-300 p-4`}
+                        >
+                          <MarkdownRenderer content={content} />
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
