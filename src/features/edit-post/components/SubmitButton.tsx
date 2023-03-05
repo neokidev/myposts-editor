666a1121ad5a7d775a21ac6b54ff60a00132536b
ot07 <ot07neoki@gmail.com>
2023-03-05 09:41:58 +0900

    include published status in createPost mutation

--- src/features/edit-post/components/SubmitButton.tsx
+++ src/features/edit-post/components/SubmitButton.tsx
@@ -1,9 +1,18 @@
 import { Menu, Transition } from '@headlessui/react'
-import { Fragment, useRef, useState } from 'react'
+import { type FC, Fragment, useEffect, useRef, useState } from 'react'
 
-export const SubmitButton = () => {
-  const [published, setPublished] = useState(true)
-  const publishedRef = useRef(true)
+type Props = {
+  published?: boolean
+  onChangePublished?: (published: boolean) => void
+}
+
+export const SubmitButton: FC<Props> = ({ published, onChangePublished }) => {
+  const [published_, setPublished] = useState(published ?? true)
+  const publishedRef = useRef(published ?? true)
+
+  useEffect(() => {
+    published && setPublished(published)
+  }, [published])
 
   return (
     <div className="relative flex text-right">
@@ -11,7 +20,7 @@ export const SubmitButton = () => {
         type="submit"
         className="inline-flex justify-center rounded-none rounded-l-md border-r border-blue-600 bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:z-10 focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-offset-2"
       >
-        {published ? 'Publish' : 'Draft'}
+        {published_ ? 'Publish' : 'Draft'}
       </button>
 
       <Menu>
@@ -43,7 +52,7 @@ export const SubmitButton = () => {
           leave="transition ease-in duration-75"
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
-          afterLeave={() => (publishedRef.current = published)}
+          afterLeave={() => (publishedRef.current = published_)}
         >
           <Menu.Items className="absolute right-0 top-9 mt-2 w-80 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg shadow-black/25 ring-1 ring-black ring-opacity-5 focus:outline-none">
             <Menu.Item>
@@ -53,7 +62,11 @@ export const SubmitButton = () => {
                   className={`${
                     active ? 'bg-blue-500' : ''
                   } group flex w-full rounded-t-md p-3 text-start`}
-                  onClick={() => setPublished(true)}
+                  onClick={() => {
+                    const published = true
+                    setPublished(published)
+                    onChangePublished && onChangePublished(published)
+                  }}
                 >
                   <div className="w-5">
                     {publishedRef.current && (
@@ -98,7 +111,11 @@ export const SubmitButton = () => {
                   className={`${
                     active ? 'bg-blue-500' : ''
                   } group flex w-full rounded-b-md p-3 text-start`}
-                  onClick={() => setPublished(false)}
+                  onClick={() => {
+                    const published = false
+                    setPublished(published)
+                    onChangePublished && onChangePublished(published)
+                  }}
                 >
                   <div className="w-5">
                     {!publishedRef.current && (
